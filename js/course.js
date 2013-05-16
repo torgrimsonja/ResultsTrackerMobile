7009e24a6ffddbf97859409f3f45b0280e3ee2ab
Joseph Higgins <blitzkriegmop@gmail.com>
2013-05-15 19:49:50 -0700

    course.html autogenerates list of goals, students
    CSS Wizard desperately needed.

--- js/course.js
+++ js/course.js
@@ -1,30 +1,32 @@
-$(function() {
-	if(!(courseToLoad.length > 0)){ $('#course-landing-content').append('<h1 style="text-align: center;">There\'s been a mistake. Please return from whence you came.</h1>')} 
-	else { createGoalView(false); }
-});
+
+if(!(courseToLoad.length > 0)){ $('#course-landing-content').append('<h1 style="text-align: center;">There\'s been a mistake. Please return from whence you came.</h1>')} 
+else { createGoalView(false); createStudentView(false); }
+
 
 function createGoalView(response){
-	response = $.parseJSON(response);
 	if(response){
+		response = $.parseJSON(response);
 		if(!response.error){
-			$('#goals').append($('<div data-role="collapsible-set" data-theme="'+DATA_THEME+'" data-content-theme="d" id="goalAccordion" class="ui-collapsible-set ui-corner-all"></div>'));
-			
 			var i=-1;
 			while(response[++i] != undefined){
-				var classStr;
-				switch(i){
-					case 0: 
-						classStr = 'class="ui-collapsible ui-collapsible-inset ui-collapsible-themed-content ui-collapsible-collapsed ui-first-child';
-					break;
-					
-					default:
-						classStr = 'class="ui-collapsible ui-collapsible-inset ui-collapsible-themed-content ui-collapsible-collapsed';
-					break; 
-				}
-				$('#goalAccordion').append($('<div data-role="collapsible" '+classStr+'><h3>'+response[i].name+'</h3><p>'+response[i].description+'</p></div>'));
+				$('#goals').append($('<li class="goal"><h3>'+response[i].name+'</h3></li>'));
 			}
 		}
 	} else {
 		genericAjax(createGoalView, 'requested=coursegoals&id='+courseToLoad, 'admin/mobileAjaxGate.php'); 
+	}
+}
+	
+function createStudentView(response){
+	if(response){
+		response = $.parseJSON(response);
+		if(!response.error){
+			var i=-1;
+			while(response[++i] != undefined){
+				$('#students').append($('<li class="students"><h3>'+response[i].firstName+' '+response[i].lastName+'</h3></li>'));
+			}
+		}
+	} else {
+		genericAjax(createStudentView, 'requested=students&id='+courseToLoad, 'admin/mobileAjaxGate.php'); 
 	}
 }
